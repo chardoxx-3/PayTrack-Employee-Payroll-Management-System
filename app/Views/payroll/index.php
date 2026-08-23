90e302685ec41099b21c231517c86098f252eb71
chardoxx-3 <183924151+chardoxx-3@users.noreply.github.com>
2026-08-23 14:41:59 +0800

    Updated files

--- app/Views/payroll/index.php
+++ app/Views/payroll/index.php
@@ -114,6 +114,7 @@ function peso($value) {
     $no = 1;
     $totalFirstQ = 0;
     $totalSecondQ = 0;
+    $totalMonthlyRate = 0;
     foreach($employees as $emp):
     ?>
     <?php
@@ -123,6 +124,7 @@ function peso($value) {
         $secondQ = $emp['second_quincena'] ?? round($netPay - $firstQ, 2);
         $totalFirstQ += $firstQ;
         $totalSecondQ += $secondQ;
+        $totalMonthlyRate += $emp['salary_rate'] ?? 0;
     ?>
     <tr class="border-bottom">
         <td rowspan="2" class="align-middle text-center" style="width: 40px;"><?= $no++ ?></td>
@@ -174,14 +176,19 @@ function peso($value) {
 
     <tr><td colspan="19" style="padding: 6px 0;"></td></tr>
     <tr>
-        <td colspan="16" class="text-end fw-bold">1st Quincena Total:</td>
+        <td colspan="16" class="text-end fw-bold">1st Quincena:</td>
         <td class="text-muted small border-start fw-bold"><?= peso($totalFirstQ) ?></td>
         <td colspan="2"></td>
     </tr>
     <tr>
-        <td colspan="16" class="text-end fw-bold">2nd Quincena Total:</td>
+        <td colspan="16" class="text-end fw-bold">2nd Quincena:</td>
         <td class="text-muted small border-start fw-bold"><?= peso($totalSecondQ) ?></td>
         <td colspan="2"></td>
+    </tr>
+    <tr>
+        <td colspan="4" class="text-end fw-bold">Monthly Rate Total:</td>
+        <td class="fw-bold border-start"><?= peso($totalMonthlyRate) ?></td>
+        <td colspan="14"></td>
     </tr>
     </tbody>
         </table>
