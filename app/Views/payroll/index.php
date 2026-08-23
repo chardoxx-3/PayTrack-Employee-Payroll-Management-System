6aa5d43542dffb6d4505d9ac51ba0f248570170f
chardoxx-3 <183924151+chardoxx-3@users.noreply.github.com>
2026-08-23 15:19:54 +0800

    Updated files

--- app/Views/payroll/index.php
+++ app/Views/payroll/index.php
@@ -200,12 +200,12 @@ function peso($value) {
 
     <tr class="small">
         <td colspan="16" class="text-end fw-bold">1st Quincena Total:</td>
-        <td class="text-muted border-start fw-bold"><?= peso($totalFirstQ) ?></td>
+        <td class="text-muted border-start fw-bold">₱<?= number_format($totalFirstQ, 2) ?></td>
         <td colspan="2"></td>
     </tr>
     <tr class="small">
         <td colspan="16" class="text-end fw-bold">2nd Quincena Total:</td>
-        <td class="text-muted border-start fw-bold"><?= peso($totalSecondQ) ?></td>
+        <td class="text-muted border-start fw-bold">₱<?= number_format($totalSecondQ, 2) ?></td>
         <td colspan="2"></td>
     </tr>
     <tr class="fw-bold">
